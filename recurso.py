ac6cdecaa31631288c0f199753ce2daa2eb658f9
sperrio2706 <sperrio2706@ieszaidinvergeles.org>
2023-12-17 14:01:06 +0100

    Update recurso.py

--- recurso.py
+++ recurso.py
@@ -11,12 +11,16 @@ import sys
 import re
 import os
 import subprocess
-import socket
 import datetime
 import winrm
 
 #--------------------------------------------------------------------------
 # FUNCIONES
+
+# Función que consulta la hora del sistema
+def get_datetime():
+	fecha_hora = datetime.datetime.now()
+	return fecha_hora
 
 # Función que escribe en el log
 def escribir_en_log(cadena):
@@ -34,18 +38,34 @@ def configurar(fichero):
     else:
         escribir_en_log("Archivo incorrecto introducido")
 
-# Función que comprueba si el grupo que le pasemos existe en AD
+# Función que comprueba si estas unido al dominio
+def unido_Dominio(dominio):
+    comando = "net ads info"
+    salida = subprocess.check_output(comando, shell=True)
+    salida = salida.decode("utf-8")
+    
+    
+# Función que comprueba si el grupo que le pasemos existe en AD (devuelve true o fale)
 def existe_grupo(grupo):
+    existe = False
+    comando = "getent group"
+    grupos = subprocess.check_output(comando, shell=True)
+    grupos = grupos.decode("utf-8")
+    
+    # Buscamos el grupo en el texto de salida de getent group
+    patron_realm = re.compile(r'Realm:\s+(\S+)')
+    coincidencia = patron_realm.search(grupo)
+    
+    if coincidencia:
+        existe = True
+    return existe
+    
+# Función que crea el grupo en AD
+def crea_grupo_remoto(grupo):
     # Nos conectamos por winrm a la DC (suponemos que somos trusted en el DC)
     s = winrm.Session('serverad.navidad.com', auth=('Administrador', 'Departamento1!'))
     r = s.run_cmd('ipconfig', ['/all'])
-    
-        
-    
-
-# Función que crea el grupo en AD
-def crea_grupo_remoto(grupo):
-    print("Creando grupo")
+    return r
 
 #--------------------------------------------------------------------------
 # VARIABLES
@@ -58,12 +78,18 @@ if len(sys.argv) != 3 or type(sys.argv[1]) != str or type(sys.argv[2]) != str:
 RECURSO = sys.argv[1]
 GRUPO = sys.argv[2]
 LOG = "/var/log/log_creacion_recursos"
+fecha_hora = (str(get_datetime()).split('.'))[0].replace(":", "·")
+
+# si estamos unidos al dominio
 
 # Comprobamos si el grupo especificado existe
-if not existe_grupo(GRUPO):
-    crea_grupo_remoto(GRUPO)
+#if not existe_grupo(GRUPO):
+#    crea_grupo_remoto(GRUPO)
 
-if not :
+if existe_grupo(GRUPO):
+    print ("existe")
+else:
+    print("no existe")
 
 # Platilla para añadir al fichero smb.conf
 SMB_RECURSO = f"""
@@ -77,3 +103,5 @@ create mask = 0770
 read only = no
 valid users = @{GRUPO}
 """
+
+print (SMB_RECURSO)
